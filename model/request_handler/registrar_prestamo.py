db37e9558c274a8aa480be1e7f49279b61f2ae01
andfoy <andfoy@gmail.com>
2015-09-30 19:18:01 -0500

    Deleted pyc and binary files

---
+++ model/request_handler/registrar_prestamo.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import tornado.web
+from tornado import gen
+from model.fachada import bancandes
+
+class RegistroHandler(tornado.web.RequestHandler):
+    def initialize(self, some_attribute=None):
+        self.some_attribute = some_attribute
+
+    @tornado.gen.coroutine
+    def get(self):
+    	# a = range(0, 5)
+        # b = {2:4, 5:7}
+        inst = bancandes.BancAndes.dar_instancia()
+        inst.inicializar_ruta('data/connection')
+        tipos_pa = inst.obtener_tipo_prestamo()
+        #self.render('template.html', a=a, b=b)
+        self.render('../../static/registrarPrestamo.html', tipos=tipos_pa)
+        self.redirect('/puntosAtencion')
+
+    @tornado.gen.coroutine
+    def post(self):
+        tipo = int(self.get_body_argument("tipo"))
+        localizacion = self.get_body_argument("localizacion")
+        oficina = int(self.get_body_argument("oficina"))
+
+        inst = bancandesAdmin.BancAndesAdmin.dar_instancia()
+        inst.inicializar_ruta('data/connection')
+        inst.registrar_pa(localizacion, tipo, oficina) 
+
+        # exists = inst.registrar_cuenta(tipo, idCliente, idOficina, saldo)
+        # if not exists:
+        #     self.render('../../static/registrarOficinaError.html')
+        # else:
+        # 	self.render('../../static/transaccionExitosa.html')
